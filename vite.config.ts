2ea523478ae3a9948bda741c6be257044418f8e0
Juliane Nagao <juliane@hey.com>
2023-11-20 20:35:28 +0100

    setup google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
+import {VitePluginRadar} from 'vite-plugin-radar'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+
+  console.log('debug', process.env.GA_TRACKING_ID)
+
+  return defineConfig({
+    plugins: [
+      react(),
+      VitePluginRadar({
+        analytics: {
+          id: process.env.GA_TRACKING_ID,
+        },
+      }),
+    ],
+  })
+}
